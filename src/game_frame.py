cbdf2cba7407ab5c16280c792c1627b80f5c0783
rrnax <agent47.rogal@gmail.com>
2024-03-27 07:42:35 +0100

    chess board

--- src/game_frame.py
+++ src/game_frame.py
@@ -1,3 +1,5 @@
+import math
+
 from PyQt5.QtWidgets import QFrame, QGraphicsScene, QGraphicsView, QGraphicsRectItem
 from PyQt5.QtGui import QColor
 
@@ -15,7 +17,6 @@ class GameFrame(QFrame):
                 """)
 
         self.game_scene = ChessBoard(self)
-        self.game_scene.draw_board()
         self.game_view = QGraphicsView(self.game_scene, self)
         self.game_view.setFixedSize(750, 550)
         self.game_view.setBackgroundBrush(QColor(0, 0, 0))
@@ -24,15 +25,28 @@ class GameFrame(QFrame):
     def update_size(self, new_size):
         self.setGeometry(0, 0, (new_size.width() - 1200) + 750, (new_size.height() - 820) + 550)
         self.game_view.setFixedSize((new_size.width() - 1200) + 750, (new_size.height() - 820) + 550)
+        self.game_scene.setSceneRect(0, 0, (new_size.width() - 1200) + 750, (new_size.height() - 820) + 550)
 
 
 class ChessBoard(QGraphicsScene):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setSceneRect(0, 0, 750, 550)
+        self.draw_board(490)
 
     def draw_board(self, side_length):
-        r = QGraphicsRectItem(50, 50, 50, 50)
-        r.setBrush(QColor(color_theme[3]))
-        self.addItem(r)
-        # for row in range(1, 9):
-        #     for column in range(0, 8):
+
+        field_size = math.floor(side_length/8)
+
+        start_x = math.floor(self.width()/2 - 4 * field_size)
+        start_y = math.floor(self.height()/2 - 4 * field_size)
+
+        for row in range(1, 9):
+            for column in range(0, 8):
+                if (row + column) % 2 == 0:
+                    color = QColor(color_theme[4])
+                else:
+                    color = QColor(color_theme[3])
+                field = QGraphicsRectItem(start_x + (column * field_size), start_y + (row-1) * field_size, field_size, field_size)
+                field.setBrush(color)
+                self.addItem(field)
